eef61a12d1e0203627091816ce0d84f6f5aa6d06
Arina <kostovar1601@gmail.com>
2026-04-12 14:52:45 +0500

    feat: создание страницы личного кабинета; fix: исправление стилей

--- react-parts/src/pages/PersonalAccount/PersonalAccount.jsx
+++ react-parts/src/pages/PersonalAccount/PersonalAccount.jsx
@@ -1,11 +1,15 @@
 import HeaderCream from '../../components/HeaderCream/HeaderCream';
 import Account from '../../components/Account/Account';
+import Favorite from '../../components/Favorite/Favorite';
+import MyPoints from '../../components/MyPoints/MyPoints';
 
 export default function PersonalAccount() {
     return (
         <>
         <HeaderCream />
         <Account />
+        <Favorite />
+        <MyPoints />
         </>
     )
 }
